24cf4fd85af6e32508b48af0bece91bbbd9da56d
Pedro Fonseca <pedropereira.pp264@gmail.com>
2025-02-24 20:06:54 -0300

    feat: calculo de pagamento

---
+++ Pagamento/Funcionario.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Data.SqlTypes;
+
+public class Funcionario
+{
+    public string nome;
+	private float salario;
+	private float salario_liquido;
+	public string cpf;
+
+	public Funcionario(string nome, float salario, string cpf)
+	{ 
+		this.nome = nome;
+		this.salario = salario;
+		this.cpf = cpf;
+
+		this.SetSalario(salario);
+	}
+
+	public void SetSalario(float salario)
+	{
+		this.salario = salario;
+		this.salario_liquido = salario;
+
+		if (salario > 2000.01)
+		{
+			this.salario_liquido = (float)(salario - salario * 0.075);
+		} else if (salario > 3000.01)
+		{
+			this.salario_liquido = (float)(salario - salario * 0.15);
+		} else if (salario > 4000.01)
+		{
+			this.salario_liquido = (float)(salario - salario * 0.225);
+		} else if (salario > 5000.01)
+		{
+			this.salario_liquido = (float)(salario - salario * 0.275);
+		}
+	}
+
+	public float GetSalarioLiquido()
+	{
+		return this.salario_liquido;
+	}
+
+	public float GetSalario()
+	{
+		return this.salario;
+	}
+}
---
+++ Pagamento/Program.cs
@@ -0,0 +1,23 @@
+﻿List<Funcionario> funcionarios = new List<Funcionario>();
+
+Funcionario pedro = new Funcionario("Pedro", 1253, "44648326873");
+Funcionario willian = new Funcionario("Willian", 1518, "44648326873");
+Funcionario barreto = new Funcionario("Barreto", 1517, "44648326873");
+Funcionario andre = new Funcionario("Andre", 2534, "44648326873");
+
+funcionarios.Add(pedro);
+funcionarios.Add(willian);
+funcionarios.Add(barreto);
+funcionarios.Add(andre);
+
+float salarioTotal = 0;
+float salarioLiquidoTotal = 0;
+foreach  (var funcionario in funcionarios)
+{
+    salarioLiquidoTotal += funcionario.GetSalarioLiquido();
+    salarioTotal += funcionario.GetSalario();
+}
+
+Console.WriteLine($"Salario total de todos os Funcionarios: {salarioTotal}");
+Console.WriteLine($"Salario liquido de todos os Funcionarios: {salarioLiquidoTotal}");
+Console.WriteLine($"Descontos totais {salarioTotal - salarioLiquidoTotal}");
