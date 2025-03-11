33199d7ca602f1a2a933cb68b617d12ecb00acfb
Pedro Fonseca <pedropereira.pp264@gmail.com>
2025-03-10 21:02:10 -0300

    feat: rpg class

---
+++ Rpg/Personagem.cs
@@ -0,0 +1,26 @@
+﻿public class Personagem
+{
+	public int Vida { get; set; } 
+
+	public string Name { get; private set; }
+	public int Ataque { get; private set; }
+	public int Defesa { get; private set; }
+
+	public Personagem(string name, int ataque, int defesa) 
+	{
+		Ataque = ataque;
+		Defesa = defesa;
+		Name = name;
+		Vida = 100;
+	}
+
+	public void AtacarPersonagem(Personagem alvo)
+	{
+		int dano = Ataque - Defesa;
+
+		if (dano == 0 || alvo.Vida == 0) return;
+
+		if (alvo.Vida - dano < 0) alvo.Vida = 0;
+		else alvo.Vida -= dano;
+	}
+}
---
+++ Rpg/Program.cs
@@ -0,0 +1,6 @@
+﻿Personagem pedro = new Personagem("Pedro", 100, 20);
+Personagem barreto = new Personagem("Barreto", 20, 10);
+
+Console.WriteLine(barreto.Vida);
+pedro.AtacarPersonagem(barreto);
+Console.WriteLine(barreto.Vida);
